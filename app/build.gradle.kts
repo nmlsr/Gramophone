6a371e0466f31b0857a81f8424e23cafe6152b04
QwQ3094 <100106591+nmlsr@users.noreply.github.com>
2025-02-11 10:10:56 +0800

    RikkaX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -262,6 +262,7 @@ dependencies {
     implementation("androidx.transition:transition-ktx:1.5.1") // <-- for predictive back TODO can we remove explicit dep now?
     implementation("com.mikepenz:aboutlibraries:$aboutLibsVersion")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("dev.rikka.rikkax.material:material-preference:2.0.0")
     implementation("me.zhanghai.android.fastscroll:library:1.3.0")
     implementation("io.coil-kt.coil3:coil:3.0.4")
     //noinspection GradleDependency newer versions need java.nio which is api 26+
